2b370620f0d16aa906ae9dc7b78a64478d03aa93
owner <themichellemcguire@gmail.com>
2019-01-27 21:15:46 -0600

    initial add for realz

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+/*----- constants -----*/ 
+
+
+
+
+/*----- app's state (variables) -----*/ 
+let board;
+
+
+
+/*----- cached element references -----*/ 
+const squares= document.querySelectorAll('#board div');
+
+
+
+/*----- event listeners -----*/ 
+
+
+
+/*----- functions -----*/
+function init() {}
+    board = [
+        
+    ];
+    render();
+};
+
+function handleMove() {
+    console.log('handleMove is ready');
+};
+function render() {
+    console.log('render is ready');
+};
+
+
+
+init();
+ console.log('hello')
